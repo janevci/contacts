fc034e23565329d7e1f2a6425437c1b106e19512
nikola.janeski <nikola.janeski@gmail.com>
2019-12-08 11:23:35 +0100

    add basic front end structure

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/custom.css') }}" />
     <title>{{ 'Contacts' }}</title>
 </head>
 <body>
-    @yield('content')
+    <div class="container-fluid">
+        <div class="row">
+          <div class="col-2">
+            @include('partials.side-menu')
+          </div>
+          <div class="col-10">
+            @yield('content')
+          </div>
+        </div>
+      </div>
 </body>
 </html>
